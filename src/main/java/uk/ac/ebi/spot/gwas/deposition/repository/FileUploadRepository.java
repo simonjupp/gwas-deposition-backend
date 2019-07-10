ba5c778ed6921ddfb810708732061142c0c8330d
Tudor Groza <tudor.groza@pryzm.health>
2019-07-10 21:45:14 +0800

    Added get file uploads implementation - https://github.com/EBISPOT/gwas-deposition-backend/issues/14

--- src/main/java/uk/ac/ebi/spot/gwas/deposition/repository/FileUploadRepository.java
+++ src/main/java/uk/ac/ebi/spot/gwas/deposition/repository/FileUploadRepository.java
@@ -3,5 +3,8 @@ package uk.ac.ebi.spot.gwas.deposition.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import uk.ac.ebi.spot.gwas.deposition.domain.FileUpload;
 
+import java.util.List;
+
 public interface FileUploadRepository extends MongoRepository<FileUpload, String> {
+    List<FileUpload> findByIdIn(List<String> ids);
 }
